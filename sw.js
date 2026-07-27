61d9b8f10c23a1f4020acced7e2f6066e0613117
thetwo02-prog <neawil0301@gmail.com>
2026-07-27 21:23:03 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'just-bead-it-v2'
+const ASSETS = ['./index.html', './manifest.json']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ))
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  // Network-first: always try to get the latest, fall back to cache if offline
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone()
+      caches.open(CACHE).then(c => c.put(e.request, copy))
+      return res
+    }).catch(() => caches.match(e.request))
+  )
+})
